d7cd0bf8988723f681c614fe644b923c76779ee5
Dasol0303 <ekthf9135@gmail.com>
2025-05-21 12:02:31 +0900

    리액트 기본 JSX

---
+++ src/Temp.js
@@ -0,0 +1,18 @@
+
+//함수형 컴포넌트 작업 -> 코드 기법 이름
+function Temp() {
+
+    return (
+        <div>
+            <h1>Temp파일</h1>
+            <h2>Temp 컴포넌트</h2>
+            <h3>내용 텍스트</h3>
+        </div>
+    );
+
+}   
+
+export default Temp; //기본으로 쓰는 익스포트 문구
+
+
+
