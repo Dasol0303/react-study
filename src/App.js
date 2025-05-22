cb3c7c36801c7908920b7a744e40aee154e05b95
Dasol0303 <ekthf9135@gmail.com>
2025-05-22 10:19:41 +0900

    연습2 state변수 활용

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import State01 from './study/State01';
 import Practice01 from './practice/Practice01';
 import State02 from './study/State02';
 import Export01 from './study/Export01';
+import Practice02 from './practice/Practice02';
 
 
 
@@ -13,8 +14,9 @@ function App() {
   // return <Basic01/>; //JSX 기본
   // return <State01/>; //useState 사용법
   // return <Practice01/>;
-  return <State02/>;
+  // return <State02/>;
   // return <Export01/>;
+  return <Practice02/>
 
   /*
   return (
