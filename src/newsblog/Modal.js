0b01ea973cf64ed0f3e76fd55525c4f8bda57e71
Dasol0303 <ekthf9135@gmail.com>
2025-05-23 14:05:38 +0900

    등록 기능 개선 (반값등록방지, 좋아요 갯수 정상작동)

--- src/newsblog/Modal.js
+++ src/newsblog/Modal.js
@@ -6,7 +6,7 @@ function Modal(props) {
 
     return (
         <div className="modal" style={{backgroundColor:props.bgColor}}>
-            <h3>{props.title} ❤ {props.likeCount}</h3>
+            <h3>{props.title} 💖 {props.likeCount}</h3>
             <p>날짜</p>
             <p>상세내용</p>
 
--- src/newsblog/NewsBlog.js
+++ src/newsblog/NewsBlog.js
@@ -20,6 +20,8 @@ function NewsBlog() {
     let [selectedTitle, setSelectedTitle] = useState('');
     let [selectedLikeCount, setSelectedLikeCount] = useState(0);
 
+    //input 값을 저장할 변수
+    let [inputText, setInputText] = useState('');
 
     return (
         <div>
@@ -28,12 +30,81 @@ function NewsBlog() {
                 <p style={ {color:"orange", fontSize:'20px'} }>{title}</p>
             </div>
 
+            <div>
+                <input type="text" id="input news" value={inputText} onChange={(event)=>{
+                    // console.log(event);
+                    console.log(event.target.value); //입력값이 어떤건지 정확하게 표기해줌
+                    setInputText(event.target.value); //State변수에 저장
+                }}></input>
+                <button onClick={()=>{
+                    //입력된 값을 제목 배열에 추가
+                    //입력된 값 : input 요소에서  onChange 가 발생할때 -> inputText state 변수에 저장되어 있음.
+                    //제목배열 news 배열변수에 추가 -> 리렌더링 -> news 배열의 갯수만큼 반복(map) 화면에 표시
+                    
+                    //1)
+                    /*
+                    //if(inputText.trim() != ''){
+                    if(inputText.trim().length > 0){
+                        let temp = [...news];
+                        temp.push(inputText);
+                        setNews(temp);
+
+                        //배열state변수 바꾸는 법 : 배열 복사 -> 값 변경/추가/삭제 -> set함수로 원래 배열에 저장
+                        setInputText(''); //공백 -> input value={inputText}
+                    } else {
+                        alert('공백은 안됩니다~');
+                        setInputText('');
+                    }
+                        */
+
+                    //2)
+                    inputText = inputText.trim();
+
+                    //if(inputText.trim().length == 0){
+                    if(inputText.length == 0){
+                        alert('공백은 안됩니다~');
+                        setInputText('');
+                        return;
+                    }
+
+                    let temp = [...news];
+                    temp.push(inputText);
+                    setNews(temp);
+
+                    likeCountArr.push(0);
+
+                    //배열state변수 바꾸는 법 : 배열 복사 -> 값 변경/추가/삭제 -> set함수로 원래 배열에 저장
+                    setInputText(''); //공백 -> input value={inputText}
+
+
+                    /* 내가 만든거
+                    if(inputText == ''){
+                        const deletedIndex = news.length;
+                        temp.splice(deletedIndex, 1);
+                        console.log(news);
+                        alert('공백은 안됩니다~');
+                    }
+                        */
+
+                    /* 순수 자바스크립트 스타일(값 입력 후 공백으로 치환)
+
+                    let title = document.getElementById('input_news').value;
+                    let temp = [...news];
+                    temp.push(inputText);
+                    setNews(temp);
+
+                    document.getElementById('input_news').value = '';
+                    */
+
+                }}>등록</button>
+            </div>
+
             {   //map
                 news.map((item, index)=>{ //item 값 안에 배열에 있는 거 들어있을거임!!
 
                     return (
 
-                            <div className='post-list'>
+                            <div className='post-list' key={index}>
                                 <h4 onClick={()=>{
                                     setShowModal(!showModal);
                                     setSelectedTitle(item);
